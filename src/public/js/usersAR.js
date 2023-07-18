bf10e9a9d2f8ed8862809d3f7d0154dd5470704e
dfralan <80047722+dfralan@users.noreply.github.com>
2023-07-18 19:32:26 -0300

    Update usersAR.js

--- src/public/js/usersAR.js
+++ src/public/js/usersAR.js
@@ -9,34 +9,10 @@ const usersAR = {
     },
     60427792846014: {
       cityID: "1989",
-      username: "@kevindevetac",
+      username: "@marbi",
       userKind: "0",
       activeInterval: "08:00/18:00(-03:00)",
       rank: "4.5",
       profileImg: "https://scontent.fros2-2.fna.fbcdn.net/v/t1.6435-9/62256802_2404539729608958_3768627338867113984_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=6RqwvCOn8PgAX9Hj6n0&_nc_ht=scontent.fros2-2.fna&oh=00_AfCptYoRA9zK1OinfA2vhnQmPq_M7j-7ZYvpm0o36St-_w&oe=64D38CAA"
-    },
-    60427792846012: {
-      cityID: "1989",
-      username: "@francossia",
-      userKind: "0",
-      activeInterval: "08:00/18:00(-03:00)",
-      rank: "3.0",
-      profileImg: "https://scontent.fros2-2.fna.fbcdn.net/v/t39.30808-6/344255982_1309057513288690_2623422176070735081_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=T4OsKmVEjogAX_aXi0_&_nc_ht=scontent.fros2-2.fna&oh=00_AfAIv2A7jwsXWnludwQVQhu1MKut-bXlD58eOg7i3Gzk9g&oe=64B33E95"
-    },
-    60427781784854: {
-      cityID: "1989",
-      username: "@facudevetac",
-      userKind: "0",
-      activeInterval: "08:00/18:00(-03:00)",
-      rank: "4.5",
-      profileImg: "https://scontent.fros2-2.fna.fbcdn.net/v/t39.30808-6/344210808_236113162432935_2413550585574849855_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=-P6bDYItXDMAX81ESMl&_nc_ht=scontent.fros2-2.fna&oh=00_AfCdSbwuxNWNDpNzithZenNtr4TqQXeZ9rGHT6fsQ0c-tA&oe=64B2CBB0"
-    },
-    60427781754: {
-      cityID: "19",
-      username: "@coopexpress_rosario",
-      userKind: "1",
-      activeInterval: "08:00/18:00(-03:00)",
-      rank: "5.0",
-      profileImg: "https://scontent.fros2-2.fna.fbcdn.net/v/t39.30808-6/308075089_511360940997403_1807329786697753045_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=8-QXBtbAb0oAX9H1Fzi&_nc_ht=scontent.fros2-2.fna&oh=00_AfA5ODMobQsOF6UjEkmmMpStT6_HBv0-h1MRH-T7vrGyaw&oe=64B472E5"
     },
   };
