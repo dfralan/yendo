5515a9b6a394156e06d637666b4409a29fc9857e
dfralan <dfralan@gmail.com>
2023-07-19 02:14:12 -0300

    upt

--- sw.js
+++ sw.js
@@ -42,33 +42,42 @@ self.addEventListener('activate', function(event) {
 
 // Intercepción de las solicitudes
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Si la solicitud está en caché, devuelve la respuesta en caché
-        if (response) {
-          return response;
-        }
-
-        // Si la solicitud no está en caché, realiza la solicitud a la red
-        return fetch(event.request)
-          .then(function(response) {
-            // Si la respuesta es exitosa y es una solicitud GET, almacena la respuesta en caché
-            if (response && response.status === 200 && event.request.method === 'GET') {
-              var responseToCache = response.clone();
-
-              caches.open(CACHE_NAME)
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-            }
-
+    // Ignora las solicitudes de extensiones de Chrome
+    if (event.request.url.startsWith('chrome-extension://')) {
+      return;
+    }
+  
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Si la solicitud está en caché, devuelve la respuesta en caché
+          if (response) {
             return response;
-          })
-          .catch(function() {
-            // Si la solicitud falla, muestra la vista offline.html
-            return caches.match('/offline.html');
-          });
-      })
-  );
-});
+          }
+  
+          // Si la solicitud no está en caché, realiza la solicitud a la red
+          return fetch(event.request)
+            .then(function(response) {
+              // Si la respuesta es exitosa y es una solicitud GET, almacena la respuesta en caché
+              if (response && response.status === 200 && event.request.method === 'GET') {
+                var responseToCache = response.clone();
+  
+                caches.open(CACHE_NAME)
+                  .then(function(cache) {
+                    // Ignora las solicitudes de extensiones de Chrome al almacenar en caché
+                    if (!event.request.url.startsWith('chrome-extension://')) {
+                      cache.put(event.request, responseToCache);
+                    }
+                  });
+              }
+  
+              return response;
+            })
+            .catch(function() {
+              // Si la solicitud falla, muestra la vista offline.html
+              return caches.match('/offline.html');
+            });
+        })
+    );
+  });
+  
