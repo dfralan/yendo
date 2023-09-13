2747ac64e67360aa94dd824a84c4f1b645d25d8a
@dfralan <gastondufourok@gmail.com>
2023-09-13 08:56:24 -0300

    commit

--- partner/src/app.js
+++ partner/src/app.js
@@ -4,3 +4,25 @@ function clickOnTeller(){
       buttonFatherBrick.click();
     }
 }
+
+function toggleAccessPassModal(){
+    const accessPassModal = document.getElementById('accessPassModal');
+    if (accessPassModal.classList.contains("display-none")){
+      accessPassModal.classList.remove("display-none");
+    } else {
+      accessPassModal.classList.add("display-none");
+    }
+}
+
+function toggleClientZone(){
+    const clientZone = document.getElementById('clientZone');
+    if (clientZone.classList.contains("display-none")){
+      clientZone.classList.remove("display-none");
+    } else {
+      clientZone.classList.add("display-none");
+    }
+}
+
+function attemptAccessPass() {
+  
+}
