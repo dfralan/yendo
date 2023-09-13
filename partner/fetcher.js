9d1d4fe31c70bd2e3e52259981fa863fc1ce1167
@dfralan <gastondufourok@gmail.com>
2023-09-12 23:57:12 -0300

    commit

--- partner/fetcher.js
+++ partner/fetcher.js
@@ -4,23 +4,66 @@ const sheetName = 'users';
 let qu = 'SELECT A WHERE ROW >= 4 AND ROW <= 200';
 const query = encodeURIComponent(qu);
 const url = `${base}&sheet=${sheetName}&tq=${query}`;
-
+const data = [];
 document.addEventListener('DOMContentLoaded', init);
+
+const output = document.querySelector('.output');
 
 function init() {
     console.log('ready');
     fetch(url)
         .then(res => res.text())
         .then(rep => {
-            const start = rep.indexOf('{"cols":[');
-            const end = rep.indexOf('}]}');
-            if (start !== -1 && end !== -1) {
-                const jsonResponse = JSON.parse(rep.substring(start, end + 2));
-                const data = jsonResponse.rows.map(row => row.c[0].v); // Extract data from the first column (A)
-                console.log(data);
-            }
+            //console.log(rep);
+            const jsData = JSON.parse(rep.substr(47).slice(0, -2));
+            console.log(jsData);
+            const colz = [];
+            jsData.table.cols.forEach((heading) => {
+                if (heading.label) {
+                    colz.push(heading.label.toLowerCase().replace(/\s/g, ''));
+                }
+            })
+            jsData.table.rows.forEach((main) => {
+                //console.log(main);
+                const row = {};
+                colz.forEach((ele, ind) => {
+                    //console.log(ele);
+                    row[ele] = (main.c[ind] != null) ? main.c[ind].v : '';
+                })
+                data.push(row);
+            })
+            maker(data);
         })
-        .catch(error => {
-            console.error(error);
-        });
+}
+
+function maker(json) {
+    const div = document.createElement('div');
+    div.style.display = 'grid';
+
+    output.append(div);
+    let first = true;
+    json.forEach((el) => {
+        //console.log(ele);
+        const keys = Object.keys(el);
+        div.style.gridTemplateColumns = `repeat(${keys.length} ,1fr)`;
+        if (first) {
+            first = false;
+            keys.forEach((heading) => {
+                const ele = document.createElement('div');
+                ele.textContent = heading.toUpperCase();
+                ele.style.background = 'black';
+                ele.style.color = 'white';
+                div.append(ele);
+            })
+
+        }
+        keys.forEach((key) => {
+            const ele = document.createElement('div');
+            ele.style.border = '1px solid #ddd';
+            ele.textContent = el[key];
+            div.append(ele);
+        })
+        console.log(keys);
+    })
+
 }
