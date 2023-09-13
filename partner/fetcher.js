429afeae593299564b2308ba331c6ad9981bec9c
@dfralan <gastondufourok@gmail.com>
2023-09-13 00:28:17 -0300

    h

--- partner/fetcher.js
+++ partner/fetcher.js
@@ -1,7 +1,7 @@
 const sheetID = '1CoqgRxK1WOkYsdZ6SPAUl9iWN2fEKm27TpzJ8YBvuLY';
 const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
 const sheetName = 'yendo - plan inicial';
-let qu = 'Select A';
+let qu = 'Select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T';
 const query = encodeURIComponent(qu);
 const url = `${base}&sheet=${sheetName}&tq=${query}`;
 const data = [];
@@ -17,53 +17,5 @@ function init() {
             //console.log(rep);
             const jsData = JSON.parse(rep.substr(47).slice(0, -2));
             console.log(jsData);
-            const colz = [];
-            jsData.table.cols.forEach((heading) => {
-                if (heading.label) {
-                    colz.push(heading.label.toLowerCase().replace(/\s/g, ''));
-                }
-            })
-            jsData.table.rows.forEach((main) => {
-                //console.log(main);
-                const row = {};
-                colz.forEach((ele, ind) => {
-                    //console.log(ele);
-                    row[ele] = (main.c[ind] != null) ? main.c[ind].v : '';
-                })
-                data.push(row);
-            })
-            maker(data);
         })
-}
-
-function maker(json) {
-    const div = document.createElement('div');
-    div.style.display = 'grid';
-
-    output.append(div);
-    let first = true;
-    json.forEach((el) => {
-        //console.log(ele);
-        const keys = Object.keys(el);
-        div.style.gridTemplateColumns = `repeat(${keys.length} ,1fr)`;
-        if (first) {
-            first = false;
-            keys.forEach((heading) => {
-                const ele = document.createElement('div');
-                ele.textContent = heading.toUpperCase();
-                ele.style.background = 'black';
-                ele.style.color = 'white';
-                div.append(ele);
-            })
-
-        }
-        keys.forEach((key) => {
-            const ele = document.createElement('div');
-            ele.style.border = '1px solid #ddd';
-            ele.textContent = el[key];
-            div.append(ele);
-        })
-        console.log(keys);
-    })
-
 }
