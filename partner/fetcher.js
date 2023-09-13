7b26a697917dcf4e139a4ef3865f33962bb3dbe5
@dfralan <gastondufourok@gmail.com>
2023-09-13 00:04:51 -0300

    COMMIT

--- partner/fetcher.js
+++ partner/fetcher.js
@@ -1,7 +1,7 @@
 const sheetID = '1CoqgRxK1WOkYsdZ6SPAUl9iWN2fEKm27TpzJ8YBvuLY';
 const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
 const sheetName = 'yendo - plan inicial';
-let qu = 'Select A';
+let qu = 'Select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T';
 const query = encodeURIComponent(qu);
 const url = `${base}&sheet=${sheetName}&tq=${query}`;
 const data = [];
