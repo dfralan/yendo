a60da8e0edec2011bcdeccc2cab9ee296fce8a6c
@dfralan <gastondufourok@gmail.com>
2023-09-15 16:26:55 -0300

    commit

--- partner/src/menu.js
+++ partner/src/menu.js
@@ -1,54 +1,68 @@
+function generateHash(inputText) {
+  let hash = 0;
+  for (let i = 0; i < inputText.length; i++) {
+    const char = inputText.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+  }
+  return hash.toString();
+}
+
 const sheetID = '1vF3AkCM5jxXWHg-U1h_Bi2orQdc2_uBNGIJgIcX64EE';
 const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
 const sheetName = 'carta - plan inicial';
-let qu = 'Select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T';
+let qu = 'Select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y';
 const query = encodeURIComponent(qu);
 const url = `${base}&sheet=${sheetName}&tq=${query}`;
 const data = [];
-document.addEventListener('DOMContentLoaded', init);
 
-const output = document.querySelector('.output');
-
-function init() {
+(function () {
     
     fetch(url)
         .then(res => res.text())
         .then(rep => {
             const jsData = JSON.parse(rep.substr(47).slice(0, -2));
-            console.log(jsData);
 
-                        // Obtener todas las filas de la hoja de cálculo
-                        const rows = jsData.table.rows;
+            // Obtener todas las filas de la hoja de cálculo
+            const rows = jsData.table.rows;
 
-                        // Inicializar un array para almacenar los objetos de SKU
-                        const skuObjects = [];
-                        const Categories = [];
-                        const States = [];
-                        const invervalHours = [];
+            // Inicializar arrays
+            const Products = [];
+            const Categories = [];
+            const States = [];
+            const invervalHours = [];
 
-                        // Products constructor
-                        for (let i = 1; i < rows.length; i++) {
-                            const row = rows[i];
-                            const skuObject = {
-                            SKU: row.c[0] && row.c[0].v ? row.c[0].v : '',
-                            Categoria: row.c[1] && row.c[1].v ? row.c[1].v : '',
-                            Plato: row.c[2] && row.c[2].v ? row.c[2].v : '', 
-                            Description: row.c[3] && row.c[3].v ? row.c[3].v : '', 
-                            Precio: row.c[4] && row.c[4].v ? row.c[4].v : '', 
-                            Descuento: row.c[5] && row.c[5].v ? row.c[5].v : '', 
-                            Estado: row.c[6] && row.c[6].v ? row.c[6].v : '', 
-                            };
-                            skuObjects.push(skuObject);
-                            
-                        }
+            // Construct products (Columns A to G)
+            for (let i = 1; i < rows.length; i++) {
+                const row = rows[i];
+                const Products = {
+                    SKU: row.c[0] && row.c[0].v ? row.c[0].v : '',
+                    Categoria: row.c[1] && row.c[1].v ? row.c[1].v : '',
+                    Plato: row.c[2] && row.c[2].v ? row.c[2].v : '', 
+                    Description: row.c[3] && row.c[3].v ? row.c[3].v : '', 
+                    Precio: row.c[4] && row.c[4].v ? row.c[4].v : '', 
+                    Descuento: row.c[5] && row.c[5].v ? row.c[5].v : '', 
+                    Estado: row.c[6] && row.c[6].v ? row.c[6].v : '', 
+                    };
+                    Products.push(Products);
+            }
 
-                        // Product iterator example
-                        skuObjects.forEach((skuObject) => {
-                        console.log('SKU:', skuObject.SKU);
-                        console.log('Categoría:', skuObject.Categoria);
-                        console.log('Plato:', skuObject.Plato);
-                        // Agrega otras propiedades según tus necesidades
-                        });
+            // Construct Categories Array (Column I)
+            for (let i = 1; i < rows.length; i++) {
+                const category = rows[i].c[8] && rows[i].c[8].v ? rows[i].c[8].v : '';
+                Categories.push(category);
+            }
+
+            Products.forEach((Products) => {
+                console.log('SKU:', Products.SKU);
+                console.log('Categoría:', Products.Categoria);
+                console.log('Plato:', Products.Plato);
+            });
+
+            // Log the extracted categories
+            Categories.forEach((category, index) => {
+                console.log(`Category ${index + 1}: ${category}`);
+            });
 
         })
-}
+
+})();
