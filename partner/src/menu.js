db0d4540b8c7d8e045bf1ab289fcc5f460c9d85d
@dfralan <gastondufourok@gmail.com>
2023-09-15 12:49:11 -0300

    commit

--- partner/src/menu.js
+++ partner/src/menu.js
@@ -17,39 +17,39 @@ function init() {
             const jsData = JSON.parse(rep.substr(47).slice(0, -2));
             console.log(jsData);
 
-                        // Obtener todas las filas de la hoja de cálculo
-                        const rows = jsData.table.rows;
+            // Obtener todas las filas de la hoja de cálculo
+            const rows = jsData.table.rows;
 
-                        // Inicializar un array para almacenar los objetos de SKU
-                        const skuObjects = [];
-                        const Categories = [];
-                        const States = [];
-                        const invervalHours = [];
+            // Inicializar un array para almacenar los objetos de SKU
+            const skuObjects = [];
+            const Categories = [];
+            const States = [];
+            const invervalHours = [];
 
-                        // Products constructor
-                        for (let i = 1; i < rows.length; i++) {
-                        const row = rows[i];
-                            if (row.c && row.c[0] && row.c[0].v) {
-                                const skuObject = {
-                                SKU: row.c[0].v,
-                                Categoria: row.c[1] ? row.c[1].v : '',
-                                Plato: row.c[2] ? row.c[2].v : '', 
-                                Description: row.c[3] ? row.c[3].v : '', 
-                                Precio: row.c[4] ? row.c[4].v : '', 
-                                Descuento: row.c[5] ? row.c[5].v : '', 
-                                Estado: row.c[6] ? row.c[6].v : '', 
-                                };
-                                skuObjects.push(skuObject);
-                            }
-                        }
+            // Products constructor
+            for (let i = 1; i < rows.length; i++) {
+            const row = rows[i];
+                if (row.c && row.c[0] && row.c[0].v) {
+                    const skuObject = {
+                    SKU: row.c[0].v,
+                    Categoria: row.c[1] ? row.c[1].v : '',
+                    Plato: row.c[2] ? row.c[2].v : '', 
+                    Description: row.c[3] ? row.c[3].v : '', 
+                    Precio: row.c[4] ? row.c[4].v : '', 
+                    Descuento: row.c[5] ? row.c[5].v : '', 
+                    Estado: row.c[6] ? row.c[6].v : '', 
+                    };
+                    skuObjects.push(skuObject);
+                }
+            }
 
-                        // Product iterator example
-                        skuObjects.forEach((skuObject) => {
-                        console.log('SKU:', skuObject.SKU);
-                        console.log('Categoría:', skuObject.Categoria);
-                        console.log('Plato:', skuObject.Plato);
-                        // Agrega otras propiedades según tus necesidades
-                        });
+            // Product iterator example
+            skuObjects.forEach((skuObject) => {
+            console.log('SKU:', skuObject.SKU);
+            console.log('Categoría:', skuObject.Categoria);
+            console.log('Plato:', skuObject.Plato);
+            // Agrega otras propiedades según tus necesidades
+            });
 
         })
 }
