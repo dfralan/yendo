9cb72386e85ef173282735fec3b9fb7d7e3d02da
@dfralan <gastondufourok@gmail.com>
2023-09-16 10:10:56 -0300

    commit

--- partner/src/menu.js
+++ partner/src/menu.js
@@ -16,7 +16,7 @@ function extractUserFromUrl(url) {
     return null;
 }
 
-const sheetID = '1vF3AkCM5jxXWHg-U1h_Bi2orQdc2_uBNGIJgIcX64EE';
+var sheetID = '';
 const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
 const sheetName = 'carta - plan inicial';
 let qu = 'Select A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y';
@@ -30,63 +30,62 @@ const data = [];
     const storedURL = localStorage.getItem('currentUrl');
     const userName = extractUserFromUrl(storedURL)
     const userHash = generateHash(userName)
-    console.log(storedURL)
-    console.log(userName)
-    console.log(userHash)
-    if (partnersAR[userHash]){
-        console.log('there is user here')
-    }
+    if (!partnersAR[userHash]){
+        window.location.href = "https://yendo.delivery/partner";
+    } else {
+        sheetID = partnersAR[userHash]?.menuId
     
-    // Fetch menu from user
-    fetch(url)
-        .then(res => res.text())
-        .then(rep => {
-            const jsData = JSON.parse(rep.substr(47).slice(0, -2));
+        // Fetch menu from user
+        fetch(url)
+            .then(res => res.text())
+            .then(rep => {
+                const jsData = JSON.parse(rep.substr(47).slice(0, -2));
 
-            // Obtener todas las filas de la hoja de cálculo
-            const rows = jsData.table.rows;
+                // Obtener todas las filas de la hoja de cálculo
+                const rows = jsData.table.rows;
 
-            // Inicializar arrays
-            const Products = [];
-            const Categories = [];
-            const invervalHours = [];
+                // Inicializar arrays
+                const Products = [];
+                const Categories = [];
+                const invervalHours = [];
 
-            // Construct products (Columns A to G)
-            for (let i = 1; i < rows.length; i++) {
-                const row = rows[i];
-                const Product = {
-                    SKU: row.c[0] && row.c[0].v ? row.c[0].v : '',
-                    Categoria: row.c[1] && row.c[1].v ? row.c[1].v : '',
-                    Plato: row.c[2] && row.c[2].v ? row.c[2].v : '', 
-                    Description: row.c[3] && row.c[3].v ? row.c[3].v : '', 
-                    Precio: row.c[4] && row.c[4].v ? row.c[4].v : '', 
-                    Descuento: row.c[5] && row.c[5].v ? row.c[5].v : '', 
-                    Estado: row.c[6] && row.c[6].v ? row.c[6].v : '', 
-                    };
-                    Products.push(Product);
-            }
-
-            // Construct Categories Array (Column I)
-            for (let i = 1; i < rows.length; i++) {
-                const category = rows[i].c[8] && rows[i].c[8].v !== null && rows[i].c[8].v !== undefined && rows[i].c[8].v !== '' ? rows[i].c[8].v : null;
-
-                // Only push non-empty categories
-                if (category !== null) {
-                    Categories.push(category);
+                // Construct products (Columns A to G)
+                for (let i = 1; i < rows.length; i++) {
+                    const row = rows[i];
+                    const Product = {
+                        SKU: row.c[0] && row.c[0].v ? row.c[0].v : '',
+                        Categoria: row.c[1] && row.c[1].v ? row.c[1].v : '',
+                        Plato: row.c[2] && row.c[2].v ? row.c[2].v : '', 
+                        Description: row.c[3] && row.c[3].v ? row.c[3].v : '', 
+                        Precio: row.c[4] && row.c[4].v ? row.c[4].v : '', 
+                        Descuento: row.c[5] && row.c[5].v ? row.c[5].v : '', 
+                        Estado: row.c[6] && row.c[6].v ? row.c[6].v : '', 
+                        };
+                        Products.push(Product);
                 }
-            }
 
-            Products.forEach((Products) => {
-                console.log('SKU:', Products.SKU);
-                console.log('Categoría:', Products.Categoria);
-                console.log('Plato:', Products.Plato);
-            });
+                // Construct Categories Array (Column I)
+                for (let i = 1; i < rows.length; i++) {
+                    const category = rows[i].c[8] && rows[i].c[8].v !== null && rows[i].c[8].v !== undefined && rows[i].c[8].v !== '' ? rows[i].c[8].v : null;
 
-            // Log the extracted categories
-            Categories.forEach((category, index) => {
-                console.log(`Category ${index + 1}: ${category}`);
-            });
+                    // Only push non-empty categories
+                    if (category !== null) {
+                        Categories.push(category);
+                    }
+                }
 
-        })
+                Products.forEach((Products) => {
+                    console.log('SKU:', Products.SKU);
+                    console.log('Categoría:', Products.Categoria);
+                    console.log('Plato:', Products.Plato);
+                });
+
+                // Log the extracted categories
+                Categories.forEach((category, index) => {
+                    console.log(`Category ${index + 1}: ${category}`);
+                });
+
+            })
+    }
 
 })();
