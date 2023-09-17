a3369b806674e4875d1e63985a81abccc887ee5b
@dfralan <gastondufourok@gmail.com>
2023-09-17 02:02:17 -0300

    d

--- partner/src/menu.js
+++ partner/src/menu.js
@@ -96,7 +96,7 @@ function generateHash(inputText) {
                     newCategory.classList.add('responsive-2', 'display-flex', 'flex-col', 'padded', 'no-padded-left', 'no-padded-right');
 
                     let categoryElement = `
-                        <h2 style="background-color: #1B406B;" class="w-fit color-white padded font-500 max-width-100">
+                        <h2 style="background-color: #1B406B;" class="w-fit color-white padded font-500 max-width-100 wrap">
                             ${category}
                         </h2>
                         <div class="productContainer w-100 display-flex flex-col">
