d42e756e6ea9b5050ea42134642e6308f58a0e41
@dfralan <gastondufourok@gmail.com>
2023-09-18 08:27:23 -0300

    commit

--- partner/src/users/partnersAR.js
+++ partner/src/users/partnersAR.js
@@ -10,7 +10,7 @@ const partnersAR = {
       subscriptionKind: "partner",
       profileImg: "https://lh5.googleusercontent.com/p/AF1QipMePfh29x8ZmgnBY4MxxA4zCUOKElAZmyWmrsTI=w203-h114-k-no",
       menuId: '1vF3AkCM5jxXWHg-U1h_Bi2orQdc2_uBNGIJgIcX64EE',
-      tellerId: '1vF3AkCM5jxXWHg-U1h_Bi2orQdc2_uBNGIJgIcX64EE',
+      tellerForm: 'https://docs.google.com/forms/d/e/1FAIpQLScWvqoWZtQIH4Akp_s-RmUveN-XyjqmS7s7jzkZChwCgfC2-g/viewform?usp=pp_url&entry.1324983604=a&entry.514815962=b&entry.99634262=c&entry.892467723=d',
       tintColor: '#1B406B',
       contrastColor: 'white',
     }
