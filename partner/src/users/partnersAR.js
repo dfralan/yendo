d46355cc299fd64f65aef5c55a16f4f799ee33ef
@dfralan <gastondufourok@gmail.com>
2023-09-22 20:02:45 -0300

    commit

--- partner/src/users/partnersAR.js
+++ partner/src/users/partnersAR.js
@@ -1,17 +1,17 @@
 const partnersAR = {
-    5025842401: {
-      cityID: "3542",
-      username: "casablanca-rosario",
-      userKind: "1",
-      categories: ["017"],
-      locations: ["-32.8739917,-60.7587183"],
-      activeInterval: "09:00/02:00(-03:00)",
-      rank: "4.0",
-      subscriptionKind: "partner",
-      profileImg: "https://lh5.googleusercontent.com/p/AF1QipMePfh29x8ZmgnBY4MxxA4zCUOKElAZmyWmrsTI=w203-h114-k-no",
-      menuId: '1vF3AkCM5jxXWHg-U1h_Bi2orQdc2_uBNGIJgIcX64EE',
-      tellerForm: 'https://docs.google.com/forms/d/e/1FAIpQLScWvqoWZtQIH4Akp_s-RmUveN-XyjqmS7s7jzkZChwCgfC2-g/viewform?usp=pp_url&entry.1324983604=a&entry.514815962=b&entry.99634262=c&entry.892467723=d',
-      tintColor: '#1B406B',
-      contrastColor: 'white',
-    }
+  5025842401: {
+    cityID: "3542",
+    username: "casablanca-rosario",
+    userKind: "1",
+    categories: ["017"],
+    locations: ["-32.8739917,-60.7587183"],
+    activeInterval: "09:00/02:00(-03:00)",
+    rank: "4.0",
+    subscriptionKind: "partner",
+    profileImg: "https://lh5.googleusercontent.com/p/AF1QipMePfh29x8ZmgnBY4MxxA4zCUOKElAZmyWmrsTI=w203-h114-k-no",
+    menuId: '1vF3AkCM5jxXWHg-U1h_Bi2orQdc2_uBNGIJgIcX64EE',
+    tellerForm: 'https://docs.google.com/forms/d/e/1FAIpQLScWvqoWZtQIH4Akp_s-RmUveN-XyjqmS7s7jzkZChwCgfC2-g/viewform?usp=pp_url&entry.1324983604=a&entry.514815962=b&entry.99634262=c&entry.892467723=d',
+    tintColor: '#1B406B',
+    contrastColor: 'white',
+  }
 };
