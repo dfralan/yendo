372cd13814afee739c5cea0ec0bef3dbe5f7ece9
@dfralan <gastondufourok@gmail.com>
2023-09-23 00:02:34 -0300

    c

--- src/public/js/record.js
+++ src/public/js/record.js
@@ -1,10 +1,11 @@
 
 
 function saveUrl() {
-    var currentUrl = window.location.href; // Get the current URL
     // Delete previously saved cookie (if any)
     localStorage.removeItem('currentUrl');
-    // Save the current URL to Local Storage
+    // Set the current URL
+    const currentUrl = window.location.href;
+    // Save it to Local Storage
     localStorage.setItem('currentUrl', currentUrl);
 }
 saveUrl()
