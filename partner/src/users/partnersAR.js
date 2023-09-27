1043f4de375c3e6f8ce5f1abb6f4b92cf4c6ae81
dfralan <dfralan@gmail.com>
2023-09-27 13:11:54 -0300

    commit

--- partner/src/users/partnersAR.js
+++ partner/src/users/partnersAR.js
@@ -1,5 +1,6 @@
 const partnersAR = {
   5025842401: {
+    subscriptionKind: "partner",
     cityID: "3542",
     username: "casablanca-rosario",
     userKind: "1",
@@ -7,7 +8,6 @@ const partnersAR = {
     locations: ["-32.8739917,-60.7587183"],
     activeInterval: "09:00/02:00(-03:00)",
     rank: "4.0",
-    subscriptionKind: "partner",
     profileImg: "https://lh5.googleusercontent.com/p/AF1QipMePfh29x8ZmgnBY4MxxA4zCUOKElAZmyWmrsTI=w203-h114-k-no",
     menuId: '1vF3AkCM5jxXWHg-U1h_Bi2orQdc2_uBNGIJgIcX64EE',
     tellerForm: 'https://docs.google.com/forms/d/e/1FAIpQLScWvqoWZtQIH4Akp_s-RmUveN-XyjqmS7s7jzkZChwCgfC2-g/viewform?usp=pp_url&entry.1324983604=a&entry.514815962=b&entry.99634262=c&entry.892467723=d',
@@ -17,6 +17,7 @@ const partnersAR = {
     wspNumber: '5493415807460'
   },
   5624572006: {
+    subscriptionKind: "inicial",
     cityID: "3538",
     username: "bien-de-aca-pueblo-esther",
     userKind: "1",
@@ -24,7 +25,6 @@ const partnersAR = {
     locations: ["-32.8739917,-60.7587183"],
     activeInterval: "09:00/02:00(-03:00)",
     rank: "5.0",
-    subscriptionKind: "inicial",
     profileImg: "https://lh5.googleusercontent.com/p/AF1QipMePfh29x8ZmgnBY4MxxA4zCUOKElAZmyWmrsTI=w203-h114-k-no",
     menuId: '1fp8703N19NkmTkjCcYBTrxsH8gu7--f9vilk2sjgZGI',
     tintColor: '#FFFFFF',
