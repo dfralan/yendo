f0bd88bd4f5b4b9f883250a2a25a3b164898ba6f
dfralan <dfralan@gmail.com>
2023-09-27 13:08:04 -0300

    commit

--- partner/src/menu.js
+++ partner/src/menu.js
@@ -152,10 +152,11 @@ function generateHash(inputText) {
                     let categoryHash = generateHash(category)
 
                     const newCategoryAccessButton = document.createElement('a')
-                    newCategoryAccessButton.classList.add('s-padded', 'color-white', 'decoration-none', 'no-wrap');
+                    newCategoryAccessButton.classList.add('s-padded', 'decoration-none', 'no-wrap');
                     newCategoryAccessButton.setAttribute('href', ('#'+categoryHash))
                     newCategoryAccessButton.innerText = category;
                     newCategoryAccessButton.style.backgroundColor = partnerTintColor;
+                    newCategoryAccessButton.style.color = partnerContrastColor;
 
                     const newCategory = document.createElement('div');
                     newCategory.id = categoryHash
