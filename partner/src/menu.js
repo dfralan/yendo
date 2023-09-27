2b1b78028486d02e132c41c3506e6774fffefab2
dfralan <dfralan@gmail.com>
2023-09-27 16:52:19 -0300

    commit

--- partner/src/menu.js
+++ partner/src/menu.js
@@ -206,7 +206,7 @@ function generateHash(inputText) {
                                     return `<span style='color: ${partnerAccentColor}' class='font-300'> [${a}]</span>`
                                 }
                                 if (partnerStyle == 'modern'){
-                                    return `<span style='color: ${partnerContrastColor}; background-color: ${partnerAccentColor}' class='font-300 rounded-s xs-padded'> ${a}</span>`
+                                    return `<span style='color: ${partnerContrastColor}; background-color: ${partnerAccentColor}' class='font-300 xs-padded'> ${a}</span>`
                                 }
                             }
 
