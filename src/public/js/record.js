74670ce3f32460362fda5fd560d258790dd13006
dfralan <dfralan@gmail.com>
2023-09-28 09:46:28 -0300

    commit

--- src/public/js/record.js
+++ src/public/js/record.js
@@ -3,8 +3,16 @@
 function saveUrl() {
     // Delete previously saved cookie (if any)
     localStorage.removeItem('currentUrl');
+
     // Set the current URL
     const currentUrl = window.location.href;
+
+    // Redirects to partner menu area if it fits
+    if (currentUrl.includes("partner")) {
+        window.location.href = "https://yendo.delivery/partner/menu.html";
+        return
+    }
+    
     // Save it to Local Storage
     localStorage.setItem('currentUrl', currentUrl);
 }
