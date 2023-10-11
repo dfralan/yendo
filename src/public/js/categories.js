b5a7f8c3fb252046f1616a65f8dbfddf9f2b1bae
dfralan <dfralan@gmail.com>
2023-10-11 01:14:04 -0300

    commit

--- src/public/js/categories.js
+++ src/public/js/categories.js
@@ -216,11 +216,11 @@ function toggleMe(element) {
   if (element.classList.contains('selected')) {
     element.classList.remove('selected');
     element.classList.remove('color-white');
-    element.classList.add('bg-black');
+    element.style.backgroundColor = ''; 
   } else {
     element.classList.add('selected');
     element.classList.add('color-white');
-    element.classList.remove('bg-black');
+    element.style.backgroundColor = '#0D7A5F'; 
   }
 }
 
