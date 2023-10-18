a249f81773a71d532ed796b688f30497647f071f
dfralan <dfralan@gmail.com>
2023-10-18 18:19:54 -0300

    commit

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+var footerview = `
+<div class="bg-white display-flex flex-col full-center z-2 s-gap padded">
+<div class="land display-flex flex-wrap m-gap color-black">
+    <p onclick="clickOnTeller()" class="cursor-pointer decoration-none color-black">Ayuda</p>
+    <a href="https://www.linkedin.com/company/yendoapp" target="_blank" class="decoration-none color-black">Linkedin</a>
+    <a href="https://wa.me/5493412293515?text=Hola,%20quiero%20hablar%20con%20un%20asesor%20sobre%20yendo%20partner." target="_blank" class="decoration-none color-black">Whatsapp</a>
+</div>
+<p class="font-s responsive-width-66 text-center o-6 x-padded no-padded-top">© 2023 yendo.delivery. Reservados todos los derechos. "yendo partner" no es una marca registrada propiedad de yendo.delivery, ni representa una entidad legal.</p>
+</div>
+`
