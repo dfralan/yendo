5c58208e99dd01075ccec7134d9603f60399617a
dfralan <dfralan@gmail.com>
2023-10-18 09:51:47 -0300

    commit

--- src/public/js/categories.js
+++ src/public/js/categories.js
@@ -158,9 +158,9 @@ function foreach(categories) {
   }
 
   let html = `
-  <small class="no-wrap font-500 decoration-none color-secondary xs-padded">
+  <h4 class="no-wrap font-500 decoration-none color-tertiary xs-padded">
         Tendencias:
-  </small>
+  </h4>
   `;
   categories.categorias.forEach(category => {
 
@@ -168,14 +168,14 @@ function foreach(categories) {
 
     if (category.codigo === "000") {return}
     const categoryHTML = `
-      <small data-code='${category.codigo}' class="popularCategoryItem cursor-pointer no-wrap font-500 decoration-none color-white rounded-s border-solid xs-padded">
-        ${category.nombre}
-      </small>
+      <h4 data-code='${category.codigo}' class="popularCategoryItem cursor-pointer no-wrap font-500 decoration-none color-white">
+        ${category.nombre}, 
+      </h4>
     `;
     const HidedCategoryHTML = `
-      <small data-code='${category.codigo}' class="categoryItem cursor-pointer display-none no-wrap font-500 decoration-none color-white rounded-s border-solid xs-padded">
-        ${category.nombre}
-      </small>
+      <h4 data-code='${category.codigo}' class="categoryItem cursor-pointer display-none no-wrap font-500 decoration-none color-white rounded-s border-solid xs-padded">
+        ${category.nombre}, 
+      </h4>
     `;
 
     if (categoriesN > 6) {
@@ -185,7 +185,9 @@ function foreach(categories) {
     }
     
   });
-  const expandToAllCategories = `<p onclick="toggleCategories()" id='categoriesToggler' class='font-400 cursor-pointer'>Ver más</p>`
+  const expandToAllCategories = `<p onclick="toggleCategories()" id='categoriesToggler' class='font-400 cursor-pointer fill-white'>
+  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M240-400q-33 0-56.5-23.5T160-480q0-33 23.5-56.5T240-560q33 0 56.5 23.5T320-480q0 33-23.5 56.5T240-400Zm240 0q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm240 0q-33 0-56.5-23.5T640-480q0-33 23.5-56.5T720-560q33 0 56.5 23.5T800-480q0 33-23.5 56.5T720-400Z"/></svg>
+  </p>`
   html += expandToAllCategories;
 
   container.innerHTML = html;
