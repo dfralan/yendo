e1de6da7ccc0180e1687dd4c5c8464801e969b76
dfralan <dfralan@gmail.com>
2023-10-18 19:58:53 -0300

    commit

---
+++ src/components/menu.js
@@ -0,0 +1,16 @@
+var menuview = `
+<!-- Menu -->
+<div id="burguerMenu" class="display-none absolute to-top to-left width-100 height-100 z-i full-center blur-behind smooth-300 full-center text-center">
+
+  <div class="width-fit bg-body z-2 x-padded rounded brick flex-col m-gap">
+      
+    <div style="font-size: 15px !important;" class="display-flex flex-col color-black m-gap to-center overflow-scroll">
+      <a href="https://yendo.delivery/partner" style="border-width: 1px ;border-color: #0D7A5F; color: #0D7A5F;" class="width-100 no-wrap font-500 font-l decoration-none padded-wide color-black rounded-s border-solid">Ser partner</a>
+      <a href="log.html" class="cursor-pointer font-400 decoration-none color-black no-wrap">Loguearse / Registrarse</a>
+      <p onclick="clickOnTeller()" class="cursor-pointer font-400 decoration-none color-black no-wrap">¿Necesitas ayuda?</p>
+    </div>
+
+  </div>
+
+</div>
+`
--- src/components/navigator.js
+++ src/components/navigator.js
@@ -2,15 +2,15 @@ let coverImg = 'https://images.pexels.com/photos/64609/pexels-photo-64609.jpeg?a
 
 var navigatorview = `
 <!-- Find delivery section -->
-<div style='min-height: 600px' class="width-100 display-flex flex-col full-center relative">
+<div class="width-100 display-flex flex-col full-center relative">
 
     <img src=${coverImg} class="width-100 height-100 cover cover-bottom absolute to-bottom">
     <div  class="bg-black o-4 width-100 height-100 cover cover-bottom absolute to-bottom"></div>
 
-    <div class='padded no-padded-left no-padded-right display-flex full-center width-100'>
+    <div style='padding-top: 100px; padding-bottom: 20px' class='padded no-padded-left no-padded-right land display-flex full-center width-100'>
         
 
-        <div style="transition: .3s;" class="responsive-width-50 display-flex flex-col z-2 rounded-l x-padded s-gap to-center bg-white">
+        <div style="transition: .3s; max-width: 600px;" class="responsive-width-66 display-flex flex-col z-2 rounded-l x-padded s-gap to-center bg-white">
             
             <h1 class="font-600 color-primary width-100 padded no-padded-left no-padded-right">
                 Encontrá lo que buscas
@@ -45,17 +45,20 @@ function constructCategoriesDA(categories) {
     
             if (category.codigo === "000") {return}
             const categoryHTML = `
-            <h4 style='border-width: 1.5px; ; border-color: #0D7A5F; color: #0D7A5F' data-code='${category.codigo}' class="cursor-pointer no-wrap font-500 decoration-none rounded-s border-solid xs-padded popularCategoryItem">
+            <h4 style='border-width: 1.4px; ; border-color: #0D7A5F; color: #0D7A5F' data-code='${category.codigo}' class="cursor-pointer no-wrap font-500 decoration-none rounded-s border-solid xs-padded popularCategoryItem">
                 ${category.nombre}
             </h4>
             `;
             const HidedCategoryHTML = `
-            <h4 style='border-width: 1.5px; border-color: #0D7A5F; color: #0D7A5F' data-code='${category.codigo}' class="cursor-pointer no-wrap font-500 decoration-none rounded-s border-solid xs-padded categoryItem display-none">
+            <h4 style='border-width: 1.4px; border-color: #0D7A5F; color: #0D7A5F' data-code='${category.codigo}' class="cursor-pointer no-wrap font-500 decoration-none rounded-s border-solid xs-padded categoryItem display-none">
                 ${category.nombre}
             </h4>
             `;
     
-            if (categoriesN > 6) {
+            if (categoriesN > 12){
+return
+            }
+            else if (categoriesN > 4) {
             categoriesConstruction += HidedCategoryHTML;
             } else {
             categoriesConstruction += categoryHTML;
