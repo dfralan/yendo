58e9bfae90c368fc5cb6c51ead5e1ff9125865e8
dfralan <dfralan@gmail.com>
2023-10-22 12:04:05 -0300

    commit

--- src/components/heropopular.js
+++ src/components/heropopular.js
@@ -9,23 +9,23 @@ let heropopularview = `
 	</h2>
 </div>
 <div class="width-100 display-flex padded no-padded-left no-padded-right overflow-scroll">
-    <div class="display-flex flex-row l-gap land">
-			<div class="responsive-width-30 overflow-hidden display-flex flex-col s-gap rounded shadow-one">
-			<img style='height: 300px' class="height-100 cover cover-bottom to-bottom" src='https://images.pexels.com/photos/5090643/pexels-photo-5090643.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'>
+    <div class="display-flex flex-row m-gap land">
+        <div class="responsive-width-33 overflow-hidden display-flex flex-col s-gap rounded shadow-one">
+            <img style='height: 300px' class="cover cover-bottom to-bottom" src='https://images.pexels.com/photos/5090643/pexels-photo-5090643.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'>
             <div class="z-1 width-100 padded bg-white">
-            	<h1 class="font-l font-500">Electricista</h1>
-            	<p class="font-300">Precio promedio $4200 - $7200</p>
+                <h1 class="font-l font-500">Electricista</h1>
+                <p class="font-300">Precio promedio $4200 - $7200</p>
             </div>
         </div>
-        <div class="responsive-width-30 overflow-hidden display-flex flex-col s-gap rounded shadow-one">
-			<img style='height: 300px' class="height-100 cover cover-bottom to-bottom" src='https://images.pexels.com/photos/17124739/pexels-photo-17124739/free-photo-of-locksmith-lockpicking-a-car.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'>
+        <div class="responsive-width-33 overflow-hidden display-flex flex-col s-gap rounded shadow-one">
+			<img style='height: 300px' class="cover cover-bottom to-bottom" src='https://images.pexels.com/photos/17124739/pexels-photo-17124739/free-photo-of-locksmith-lockpicking-a-car.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'>
             <div class="width-100 padded bg-white">
-            <h1 class="font-l font-500">Cerrajería</h1>
-            <p class="font-300">Precio promedio $3500 - $5700</p>
+                <h1 class="font-l font-500">Cerrajería</h1>
+                <p class="font-300">Precio promedio $3500 - $5700</p>
             </div>
         </div>
-        <div class="responsive-width-30 overflow-hidden display-flex flex-col s-gap rounded shadow-one">
-			<img style='height: 300px' class="height-100 cover cover-bottom to-bottom" src='https://images.pexels.com/photos/8543572/pexels-photo-8543572.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'>
+        <div class="responsive-width-33 overflow-hidden display-flex flex-col s-gap rounded shadow-one">
+			<img style='height: 300px' class="cover cover-bottom to-bottom" src='https://images.pexels.com/photos/8543572/pexels-photo-8543572.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'>
             <div class="width-100 padded bg-white">
                 <h1 class="font-l font-500">Jardinería</h1>
                 <p class="font-300">Precio promedio $2200 - $4800</p>
