cc929683e5570d7b77b2434a09a032fe22841ba9
dfralan <dfralan@gmail.com>
2023-10-21 21:01:47 -0300

    commit

--- partner/src/users/partnersAR.js
+++ partner/src/users/partnersAR.js
@@ -50,7 +50,7 @@ const partnersAR = {
     tellerForm: 'https://docs.google.com/forms/d/e/1FAIpQLSdTpcRGfTLAUOAWI47GHOOUECMtXG6EdEgxUY3oajXqsjheNQ/viewform?usp=pp_url&entry.1168217436=a&entry.1536991342=b&entry.1697400105=c&entry.75799250=d',
     tintColor: '#232323',
     contrastColor: '#FFFFFF',
-    accentColor: '#2962E3',
+    accentColor: '#D0E0FF',
     wspNumber: '5493417462378',
     theme: 'light',
     partnerStyle: 'modern',
