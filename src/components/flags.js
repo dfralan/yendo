fe63b4c26f7810e92f9fc9972bd38f17c8dab432
dfralan <dfralan@gmail.com>
2023-10-23 08:55:34 -0300

    commit

---
+++ src/components/flags.js
@@ -0,0 +1,85 @@
+var flag1 = `
+<div class="display-flex flex-wrap width-100">
+    
+  <div class="responsive-width-50 bg-tertiary land no-padded-right bg-white full-center">
+      <div class="display-flex flex-col m-gap width-100 x-padded no-padded-left no-padded-right">
+        <h1>
+          El día a día ahora es más fácil
+        </h1>
+        <h4 class="font-400">
+          Cuando la vida se vuelve ocupada, no es necesario que la afrontes solo. Recupera tiempo para lo que amas sin tener que gastar mucho dinero.
+        </h4>
+        <ul class="list-decoration-none font-600">
+          <li class="display-flex flex-row to-start s-gap">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m382-354 339-339q12-12 28.5-12t28.5 12q12 12 12 28.5T778-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z"/></svg>
+            </span>
+            Elige tu Tasker por opiniones, habilidades y precio
+          </li>
+          <li class="display-flex flex-row to-start s-gap">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m382-354 339-339q12-12 28.5-12t28.5 12q12 12 12 28.5T778-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z"/></svg>
+            </span>
+            Programe cuando funcione para usted, tan pronto como hoy
+          </li>
+          <li class="display-flex flex-row to-start s-gap">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m382-354 339-339q12-12 28.5-12t28.5 12q12 12 12 28.5T778-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z"/></svg>
+            </span>
+            Chatea, paga, da propinas y revisa todo a través de una sola plataforma
+          </li>
+        </ul>
+      </div>
+
+  </div>
+
+  <div class="responsive-width-50 overflow-hidden">
+    <img class="width-100 height-100 cover cover-bottom to-bottom" src='https://images.pexels.com/photos/6407559/pexels-photo-6407559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'>
+  </div>
+
+</div>
+`
+
+var flag2 = `
+<div class="display-flex flex-wrap width-100">
+
+
+
+  <div class="responsive-width-50 overflow-hidden">
+    <img class="width-100 height-100 cover cover-bottom to-bottom" src='https://images.pexels.com/photos/4246108/pexels-photo-4246108.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'>
+  </div>
+    
+  <div class="responsive-width-50 bg-tertiary land no-padded-right bg-white full-center">
+      <div class="display-flex flex-col m-gap width-100 x-padded no-padded-left no-padded-right">
+        <h1>
+          Un equipo de referencia a tu alcance
+        </h1>
+        <h4 class="font-400">
+          Crea tu equipo de taskers locales con antecedentes verificados para ayudarte con la vida y de por vida. Lo que sea que necesites, lo tienen cubierto.
+        </h4>
+        <ul class="list-decoration-none font-600">
+          <li class="display-flex flex-row to-start s-gap">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m382-354 339-339q12-12 28.5-12t28.5 12q12 12 12 28.5T778-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z"/></svg>
+            </span>
+            Elige tu Tasker por opiniones, habilidades y precio
+          </li>
+          <li class="display-flex flex-row to-start s-gap">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m382-354 339-339q12-12 28.5-12t28.5 12q12 12 12 28.5T778-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z"/></svg>
+            </span>
+            Programe cuando funcione para usted, tan pronto como hoy
+          </li>
+          <li class="display-flex flex-row to-start s-gap">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m382-354 339-339q12-12 28.5-12t28.5 12q12 12 12 28.5T778-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z"/></svg>
+            </span>
+            Chatea, paga, da propinas y revisa todo a través de una sola plataforma
+          </li>
+        </ul>
+      </div>
+
+  </div>
+
+</div>
+`
